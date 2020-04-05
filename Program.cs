0bed5f965aee0652e23e69931da6838c0fa4ffa4
Sobira <sobira204@gmail.com>
2020-04-05 17:35:34 +0500

    create class Currency change class Converter

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace part1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        class Currency{
+            public double inUsd {get;set;} 
+            public double inEur {get;set;}
+            public double inRub {get;set;}
+            public double inTjs {get;set;}
+            
+        }
+        class Converter{
+            public double usd {get;set;} 
+            public double eur {get;set;}
+            public double rub {get;set;}
+            public double tjs {get;set;}
+            //public Converter (double Usd = 10.22, double Eur = 11.04, double Rub = 0.13){
+            //    inUsd = Usd;
+            //    inEur = Eur;
+            //    inRub = Rub;
+            //} 
         }
     }
 }
