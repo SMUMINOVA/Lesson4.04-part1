7b0bfa05d18521b05c55b1e82e3b09788394e0ab
Sobira <sobira204@gmail.com>
2020-04-05 18:16:52 +0500

    delete class Currancy, change class Converter, converting to tjs

--- Program.cs
+++ Program.cs
@@ -6,26 +6,35 @@ namespace part1
     {
         static void Main(string[] args)
         {
-            
-        }
-
-        class Currency{
-            public double inUsd {get;set;} 
-            public double inEur {get;set;}
-            public double inRub {get;set;}
-            public double inTjs {get;set;}
-            
+            Converter firstSum = new Converter();
+            System.Console.Write("Enter amount: ");
+            double sum = double.Parse(Console.ReadLine());
+            System.Console.WriteLine("Choose currency:\n1. USD\t2. EUR\n3. RUB\t4. TJS");
+            int choice = int.Parse(Console.ReadLine());
+            firstSum.amount = sum;
+        
+            if (choice != 4){
+                switch(choice){
+                case 1: firstSum.toTjs(firstSum.usd, firstSum.amount); break;
+                case 2: firstSum.toTjs(firstSum.eur, firstSum.amount); break;
+                case 3: firstSum.toTjs(firstSum.rub, firstSum.amount); break;
+                }
+            }
         }
         class Converter{
+            public double amount {get;set;}
             public double usd {get;set;} 
             public double eur {get;set;}
             public double rub {get;set;}
             public double tjs {get;set;}
-            //public Converter (double Usd = 10.22, double Eur = 11.04, double Rub = 0.13){
-            //    inUsd = Usd;
-            //    inEur = Eur;
-            //    inRub = Rub;
-            //} 
+            public Converter (double Usd = 10.22, double Eur = 11.04, double Rub = 0.13){
+                usd = Usd;
+                eur = Eur;
+                rub = Rub;
+            } 
+            public void toTjs(double converter, double sum){
+                System.Console.WriteLine($"in TJS: {sum*converter}"); 
+            }
         }
     }
 }
